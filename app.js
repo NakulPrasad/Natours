4bc36b4a4d4f4a734c0369dc6baacfe0e2354f04
Nakul Prasad <nakulprasad10@outlook.com>
2022-12-25 22:25:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const app = express();
 
 //routing
 app.get('/', (req, res) => {
-  res.status(200).send('Hello form server side');
+  //   res.status(200).send('Hello form server side');
+  res.status(200).json({ message: 'Hello form server side', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.send('you can post to this endpoint...');
+});
 const port = 2222;
 app.listen(port, () => {
   console.log(`App is listening on ${port}...`);
