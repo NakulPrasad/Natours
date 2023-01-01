37219133de7eb9f92becf79721f6c532531d196e
Nakul Prasad <nakulprasad10@outlook.com>
2023-01-01 19:40:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const app = express();
 
 //routing
-app.get('/', (req, res) => {
-  //   res.status(200).send('Hello form server side');
-  res.status(200).json({ message: 'Hello form server side', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   //   res.status(200).send('Hello form server side');
+//   res.status(200).json({ message: 'Hello form server side', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('you can post to this endpoint...');
-});
+// app.post('/', (req, res) => {
+//   res.send('you can post to this endpoint...');
+// });
+
+app.get('/api/v1/tours', (req, res) => {});
 
 const port = 2222;
 app.listen(port, () => {
